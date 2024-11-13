7f34992d2b9d21038c63dd903da739ae483a4d3b
Gabriella Hernandes <gbhernandes@hotmail.com>
2024-11-12 23:04:14 -0300

    adjusting responsiveness

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+document.querySelectorAll('.w-full.h-full').forEach(item => {
+    item.addEventListener('click', function () {
+      const imgSrc = this.querySelector('img').src;
+      document.getElementById('modalImage').src = imgSrc;
+      document.getElementById('modal').classList.remove('hidden');
+    });
+});
+  
+document.getElementById('closeModal').addEventListener('click', function () {
+    document.getElementById('modal').classList.add('hidden');
+});
+  
